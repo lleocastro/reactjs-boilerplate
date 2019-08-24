0b667d399754b498e4b3070e33ccad5687bd16cb
Léo Castro <leonardo_carvalho@outlook.com>
2019-08-24 00:31:45 -0300

    Configuring HTTP requests

---
+++ src/services/http.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+axios.interceptors.request.use(async (config) => {
+  config.headers.Authorization = `Bearer ${''}`; // TODO: token logic
+  return config;
+}, (err) => {
+  window.console.log(err);
+  return Promise.reject(err);
+});
+
+axios.interceptors.response.use((response) => response, (error) => {
+  if (error.response.status === 401) {
+    const requestConfig = error.config;
+    return axios(requestConfig);
+  }
+
+  return Promise.reject(error);
+});
+
+export default axios;
+
+export const graphqlQueryBuilder = (query, variables) => ({
+  query: query.replace(/\s/g, ''),
+  variables,
+});
+
+export const graphql = (query, variables, params = {}) => axios({
+  method: 'POST',
+  url: process.env.API_BASE_URL,
+  data: graphqlQueryBuilder(query, variables),
+  ...params,
+});
