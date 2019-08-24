a5e9020525c05b81a0655dbd6fe3abba11a22926
Léo Castro <leonardo_carvalho@outlook.com>
2019-08-23 23:51:02 -0300

    Added routers and propTypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import {
+  Route, Redirect, BrowserRouter, Switch,
+} from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const isAuthenticated = false; // TODO: auth logic
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) => (
+      isAuthenticated ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+      )
+    )}
+  />
+);
+
+PrivateRoute.propTypes = {
+  component: PropTypes.func.isRequired,
+  location: PropTypes.object,
+};
+
+export const Routers = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={() => <h1>Login</h1>} />
+      <PrivateRoute path="/app" component={() => <h1>App</h1>} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default function App() {
-  return (
-    <div>Hello World!</div>
-  );
+  return <Routers />;
 }
